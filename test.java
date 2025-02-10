5bf7c87a1dfe97e8c36d769767cf0f7c0b6640cb
GrantBenR <thegrantrynders@gmail.com>
2025-02-10 15:44:37 -0500

    Working Program 4, WIP program 1, test program

---
+++ test.java
@@ -0,0 +1,19 @@
+public class test 
+{
+    public static void main(String[] args)
+    {
+        Integer z = 0;
+        Integer x = 2;
+        Integer y = 4;
+        for (int i = 0; i<y; i++)
+        {
+            for (int j = 0; j<x; j++)
+            {
+                z += x;
+            }
+        }
+        System.out.println(z);
+    }
+    
+
+}
