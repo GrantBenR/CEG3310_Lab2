3fcf0fd8e89ab155c3192c0fb93e5c53811bf831
GrantBenR <thegrantrynders@gmail.com>
2025-02-11 13:27:21 -0500

    Mostly Working program 1 (Does not work for 0 exponent)

--- test.java
+++ test.java
@@ -4,15 +4,19 @@ public class test
     {
         Integer z = 0;
         Integer x = 2;
-        Integer y = 2;
+        Integer y = 0;
+        Integer k = 0;
+        k=x;
         for (int i = 0; i<y; i++)
         {
-            for (int j = 0; j<x; j++)
+            for (int j = 1; j<x; j++)
             {
-                z += x;
+                z += k;
+                System.out.println("i: " + i + ", j: " + j + ", z: " + z);
             }
+            k = z;
         }
-        System.out.println(z);
+        System.out.println("Z final: " + z);
     }
     
 
